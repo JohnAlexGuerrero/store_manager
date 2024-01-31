e478782ebe8d7c6aa994413acc9b5ec0f176d979
johnalexguerrero <johnalexguerrero@gmail.com>
2024-01-31 17:33:08 -0500

    refactory

--- provider_manager/sales/admin.py
+++ provider_manager/sales/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.shortcuts import redirect
 from django.db.models import Sum
+from decimal import Decimal
 
 from sales.models import OrderDetailSale, Order, Client
 from sales.forms import ClientForm, OrderForm, OrderDetailSaleForm
@@ -19,19 +20,42 @@ def select_one_item(modeladmin,request, queryset):
 
 @admin.register(OrderDetailSale)
 class OrderDetailSaleAdmin(admin.ModelAdmin):
-    form = OrderDetailSaleForm
+    list_display = ['product','amount','price','total','revenue','utility']
+    
+    def total(self, obj):
+        return obj.amount * obj.price
+    
+    def revenue(self, obj):
+        return (obj.price - obj.product.cost_with_tax)* obj.amount
+    
+    def utility(self, obj):
+        util = round((obj.price - obj.product.cost_with_tax) / obj.price, 2)
+        return f'{util * 100} %'
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ("createdAt","number","client","total","is_paid",'utility')
+    list_display = ("createdAt","number","client","total",'utility','revenue',"is_paid")
     form = OrderForm
     # actions = [order_selected]
     
-    def utility(self, obj):
+    def revenue(self, obj):
         if obj.is_paid:
             total = Utility.objects.filter(orderdetail__order=obj.id).aggregate(Sum('value'))
-            return total['value__sum']
+            return f'$ {total['value__sum']:,.2f}'
         return 0
+    
+    def utility(self, obj):
+        sum_utility = 0
+        num_utility = 0
+        
+        if obj.is_paid:
+            query = OrderDetailSale.objects.filter(order=obj.id)
+            num_utility = len(query)
+            for item in query:
+                sum_utility += ((item.total - (item.product.cost_with_tax * item.amount)) / item.total) * 100
+        
+            return f'{round(sum_utility / num_utility,2)} %'
+        return '0 %'
     
     def view_sale_order(modeladmin, request, queryset):
         item = select_one_item(modeladmin, request, queryset)
