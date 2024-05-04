f1fa70b8ad94c4943dfeba27561b722ebcb54d19
johnalexguerrero <johnalexguerrero@gmail.com>
2024-05-03 20:50:21 -0500

    template:invoicing:search-product

--- provider_manager/inventory/views.py
+++ provider_manager/inventory/views.py
@@ -1,6 +1,36 @@
 from django.shortcuts import render
+from django.db.models import Q
+from django.http import JsonResponse
+
 from django.views.generic import TemplateView
 
+from inventory.models import Product
+
 # Create your views here.
+
+#filtrar los productos segun su descripcion
+def searchProduct(request):
+    query = request.GET.get('q')
+    
+    products = Product.objects.filter(
+        Q(description__icontains=query)
+    )
+    
+    return JsonResponse({
+        "items": [
+            {
+                "id": item.id,
+                "description": item.description,
+                "code": item.code,
+                "und": item.unit,
+                "stock": item.stock,
+                "tax": item.tax,
+                "price": item.cost_with_tax,
+            }
+            for item in products
+        ]
+    })
+    
+
 class InventoryView(TemplateView):
     template_name = "inventory/index.html"
--- provider_manager/invoicing/urls.py
+++ provider_manager/invoicing/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 
 from invoicing.views import InvoiceCreateView
 from sales.views import SearchClientListView, clientView
+from inventory.views import searchProduct
 
 urlpatterns = [
     path('', InvoiceCreateView.as_view(), name='invoice'),
     path('search/client/', SearchClientListView, name='search-client'),
     path('client/', clientView, name='client'),
+    path('search/product/', searchProduct, name='search-product'),
 ]
